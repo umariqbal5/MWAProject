e54141c0ce308628bd402379cec07e8d6d160268
Muhammad Umar Iqbal <umarzapak5@hotmail.com>
2019-05-18 18:29:41 -0500

    Updated Angular Frontend
    Add Home Page
    Remove some unUsed Components

--- FrontEnd/src/app/pages/home/home.component.spec.ts
+++ FrontEnd/src/app/pages/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { IconsComponent } from './icons.component';
+import { HomeComponent } from './home.component';
 
-describe('IconsComponent', () => {
-  let component: IconsComponent;
-  let fixture: ComponentFixture<IconsComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IconsComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(IconsComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
