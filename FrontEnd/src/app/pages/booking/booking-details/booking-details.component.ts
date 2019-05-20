dad3f1472c347e32a2d4a4f4d405f069255811ca
THUYHONGHA <thuyhong.ip@gmail.com>
2019-05-20 00:48:36 -0500

    split bookingSchema out from userSchema and change bookingRoute, move all booking components into pages

---
+++ FrontEnd/src/app/pages/booking/booking-details/booking-details.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {BookingService} from '../../services/booking.service';
+
+@Component({
+  selector: 'app-view-booking',
+  templateUrl: './booking-details.component.html',
+  styleUrls: ['./booking-details.component.scss']
+})
+export class BookingDetailsComponent implements OnInit {
+
+  constructor(private bookingService: BookingService) { }
+  @Input() pnr;
+  booking: any;
+  ngOnInit() {
+    // View a booking with pnr
+    this.bookingService.getBookingByBookingRef(this.pnr).subscribe(data => this.booking = data );
+  }
+
+}
