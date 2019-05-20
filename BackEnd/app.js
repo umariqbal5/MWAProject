b326c040415e57564c23cb498b5ed34375a6c548
Umar <umarzapak5@hotmail.com>
2019-05-20 16:14:51 -0500

    added api/package to white list

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -30,7 +30,7 @@ const port = 4000;
 app.use(morgan('combined', { stream: accessLogStream}));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(expressJwt({secret: db_url.secrete}).unless({path: ['/api/auth/login','/api/auth/register']}));
+app.use(expressJwt({secret: db_url.secrete}).unless({path: ['/api/auth/login','/api/auth/register','/api/package']}));
 
 //========================connection=================
 
