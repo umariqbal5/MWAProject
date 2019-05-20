6e142086076b0ffa097c4f96a681c598efc5136e
Michael <mikialebachew@gmail.com>
2019-05-20 20:49:15 +0600

    typo

--- FrontEnd/src/app/components/sidebar/sidebar.component.ts
+++ FrontEnd/src/app/components/sidebar/sidebar.component.ts
@@ -12,8 +12,8 @@ export const ROUTES: RouteInfo[] = [
     { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
     { path: '/UserList', title: 'UserList',  icon:'ni-bullet-list-67 text-red', class: '' },
     { path: '/CreateUser', title: 'CreateUser', icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/bookings', title: 'Bookings', icon:'ni-single-02 text-yellow', class: '' }
-    
+    { path: '/bookings', title: 'Bookings', icon:'ni-single-02 text-yellow', class: '' },
+    { path: '/packages', title: 'Packages', icon:'ni-bullet-list-67 text-red', class: ''}
   ];
 
 @Component({
