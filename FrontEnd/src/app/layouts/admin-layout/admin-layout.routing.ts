53c1448cf18f9b9e9c5f0ed92ba4b785a6e78f79
Michael <mikialebachew@gmail.com>
2019-05-21 15:12:37 +0600

    configuration

--- FrontEnd/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ FrontEnd/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,10 +5,11 @@ import {UserProfileComponent} from '../../pages/user-profile/user-profile.compon
 import {TablesComponent} from '../../pages/tables/tables.component';
 import {AuthGuard} from '../../gaurds/auth.guard';
 import {EditUserComponent} from '../../pages/users/editUser/editUser.component';
-import {PackageListComponent} from '../../pages/packageList/packageList.component';
+import {PackageListComponent} from '../../pages/packages/packageList/packageList.component';
 import {AdminViewBookingsComponent} from '../../pages/booking/admin-view-bookings/admin-view-bookings.component';
 import {CreateUserComponent} from '../../pages/users/createUser/CreateUser.component';
 import {UserListComponent} from '../../pages/users/userList/UserList.component';
+import {CreatePackageComponent} from '../../pages/packages/createPackage/createPackage.component';
 
 export const AdminLayoutRoutes: Routes = [
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
@@ -18,6 +19,7 @@ export const AdminLayoutRoutes: Routes = [
   {path: 'createUser', component: CreateUserComponent, canActivate: [AuthGuard]},
   {path: 'packages', component: PackageListComponent, canActivate: [AuthGuard]},
   {path: 'view-bookings', component: AdminViewBookingsComponent, canActivate: [AuthGuard]},
-  {path: 'admin-view-bookings', component: AdminViewBookingsComponent}
+  {path: 'admin-view-bookings', component: AdminViewBookingsComponent},
+  {path: 'createPackage', component: CreatePackageComponent, canActivate: [AuthGuard]},
 
 ];
