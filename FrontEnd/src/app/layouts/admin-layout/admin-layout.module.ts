497b7f22031296804d8d1c33f00a477fe59aff85
Henock <henockasmelash@gmail.com>
2019-05-21 18:05:51 -0500

    pagination and search for user
    pagination and searching option for user

--- FrontEnd/src/app/layouts/admin-layout/admin-layout.module.ts
+++ FrontEnd/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,7 +5,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 //import { ReactiveFormsModule}
 import {ClipboardModule} from 'ngx-clipboard';
-
+import{NgxPaginationModule} from 'ngx-pagination';
 import {AdminLayoutRoutes} from './admin-layout.routing';
 import {DashboardComponent} from '../../pages/dashboard/dashboard.component';
 import {UserProfileComponent} from '../../pages/user-profile/user-profile.component';
@@ -18,7 +18,7 @@ import {EditUserComponent} from '../../pages/users/editUser/editUser.component';
 import {PackageListComponent} from '../../pages/packages/packageList/packageList.component';
 import {CreatePackageComponent} from '../../pages/packages/createPackage/createPackage.component';
 import {EditPackageComponent} from '../../pages/packages/editPackage/editPackage.component';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 // import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -29,7 +29,10 @@ import {EditPackageComponent} from '../../pages/packages/editPackage/editPackage
     HttpClientModule,
     NgbModule,
     ClipboardModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule
+
   ],
   declarations: [
     DashboardComponent,
